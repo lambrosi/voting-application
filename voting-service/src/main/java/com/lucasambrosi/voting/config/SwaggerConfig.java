dcbfed36a89beeecc5628f3c8fc130cb6f45cfc6
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2019-11-25 23:31:18 -0300

    Add Swagger configuration

---
+++ voting-service/src/main/java/com/lucasambrosi/voting/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.lucasambrosi.voting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lucasambrosi.voting"))
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Voting Application")
+                .description("\"Application for voting on certain topics.\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+}
