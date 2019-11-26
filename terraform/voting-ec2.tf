a12a716b6ed3403c2b43b9429c8aacf31cff089f
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2019-11-26 00:36:42 -0300

    Add terraform script

---
+++ terraform/voting-ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "voting-ec2" {
+  ami           = "ami-04b9e92b5572fa0d1"
+  instance_type = "${instance.type}"
+  vpc_security_group_ids = [aws_security_group.voting-sg.id]
+  key_name = "${key.name}"
+  
+  tags = {
+    Name = "voting-application-ec2"
+  }
+}
