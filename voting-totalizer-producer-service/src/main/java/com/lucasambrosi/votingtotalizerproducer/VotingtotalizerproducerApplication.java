f2ca2fd72b65882f6b0784295969173fac047e6e
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2019-11-25 23:20:40 -0300

    Add initial voting-totalizer-producer-service project

---
+++ voting-totalizer-producer-service/src/main/java/com/lucasambrosi/votingtotalizerproducer/VotingtotalizerproducerApplication.java
@@ -0,0 +1,17 @@
+package com.lucasambrosi.votingtotalizerproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
+public class VotingtotalizerproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingtotalizerproducerApplication.class, args);
+	}
+
+}
